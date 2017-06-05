844962b449964139fa3912feb4b7198739800d1a
Dorian Savina <d.savina@thecodingmachine.com>
2017-06-05 17:22:27 +0200

    [tests] added test on ORDER BY <aggregate function>
    Signed-off-by: Dorian Savina <d.savina@thecodingmachine.com>

--- tests/Mouf/Database/MagicQueryTest.php
+++ tests/Mouf/Database/MagicQueryTest.php
@@ -152,6 +152,9 @@ class MagicQueryTest extends \PHPUnit_Framework_TestCase
 
         $sql = 'SELECT * FROM users WHERE id IN (1, 3)';
         $this->assertEquals("SELECT * FROM users WHERE id IN (1, 3)", self::simplifySql($magicQuery->build($sql)));
+
+        $sql = 'SELECT country.* FROM country JOIN users ON country.id = users.country_id GROUP BY country.id ORDER BY COUNT(users.id) DESC';
+        $this->assertEquals('SELECT country.* FROM country JOIN users ON (country.id = users.country_id) GROUP BY country.id ORDER BY COUNT(users.id) DESC', self::simplifySql($magicQuery->build($sql)));
     }
 
     /**
